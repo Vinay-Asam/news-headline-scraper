ca572ecb834ff8ab5803e8e51e559441b1b1b2b3
VinayAsam001 <asamvinayav@gmail.com>
2023-05-04 17:01:06 -0500

    Merged local and remote repositories

---
+++ news_headlines.cs
@@ -0,0 +1,130 @@
+headline,date,source
+Canada summons Chinese ambassador over allegations of political interference,2023-05-04,CNN
+What we know about the murky drone attack on the Kremlin - and the questions that remain,2023-05-04,CNN
+Clemency pleas for Nigerian senator convicted for organ harvesting plot in UK,2023-05-04,CNN
+Sudan’s first professional stage actress killed in shelling in Khartoum,2023-05-04,CNN
+Eight school staff shot dead in northern Pakistan,2023-05-04,CNN
+Israel says killers of British-Israeli mother and daughters shot dead in Nablus,2023-05-04,CNN
+Eight killed in fire at Czech cabin used by homeless people as shelter,2023-05-04,CNN
+How the Kremlin drone attack hands Russia an opportunity,2023-05-04,CNN
+"Wave of Russian attacks on Kyiv worst in a year, Ukrainian military says",2023-05-04,CNN
+Turkey gifts Mexico puppy after rescue dog dies on earthquake duty,2023-05-04,CNN
+A school shooting has rocked Serbia. Here’s what we know,2023-05-04,CNN
+Look up to the sky for 3 space events this weekend,2023-05-04,CNN
+Saudi Arabia wants to be a bigger player in the Middle East — this time with diplomacy,2023-05-04,CNN
+Alleged Thai serial cyanide poisoner now facing at least 13 murder charges,2023-05-04,CNN
+Ecuador is in trouble and its president may pay the price,2023-05-04,CNN
+Florida Deputy helps mom deliver baby in front seat,2023-05-04,CNN
+First-ever U.S. in utero brain surgery performed in Boston,2023-05-04,CNN
+Acting legends discuss hit show,2023-05-04,CNN
+A famed Sudanese actress is killed in the crossfire as humanitarian situation worsens,2023-05-04,CNN
+"Camilla, King Charles’ longtime love, has a divisive history. What do we know about Britain’s new Queen?",2023-05-04,CNN
+Pope Francis says the Vatican is involved in a peace mission to end the war in Ukraine,2023-05-04,CNN
+"Ukrainian shelling kills four in Russian village, local governor says",2023-05-04,CNN
+Russia vows retaliation after Poland seizes embassy school,2023-05-04,CNN
+Suspected drone strike triggers massive fire in Russian-occupied Crimea,2023-05-04,CNN
+Ukraine could launch its offensive against Russia any moment. Here’s what’s waiting,2023-05-04,CNN
+Israel military and Palestinians trade fire following hunger striker’s death,2023-05-04,CNN
+Prominent Palestinian detainee Khader Adnan dies in Israeli prison after 87-day hunger strike,2023-05-04,CNN
+Turkey’s upcoming elections could unseat Erdogan. Here’s everything you need to know,2023-05-04,CNN
+"Turkey kills ISIS leader in Syria operation, Erdogan says",2023-05-04,CNN
+Netanyahu tells CNN Israel will remain a ‘robust democracy’ despite judiciary plans,2023-05-04,CNN
+"‘Even one American in Iraq is too much,’ Iran’s leader tells Iraqi president",2023-05-04,CNN
+In pictures: Britain’s King Charles III,2023-05-04,CNN
+In pictures: Charles and Camilla,2023-05-04,CNN
+In pictures: Britain’s Queen Camilla,2023-05-04,CNN
+Weird and wonderful planets beyond our solar system,2023-05-04,CNN
+"Nigeria probes popular Indomie noodles after recalls by Malaysia, Taiwan",2023-05-04,CNN
+Floods in western Rwanda kill at least 127 people,2023-05-04,CNN
+Universal Studio Group’s UCP teams up with Nigeria’s Comic Republic in new TV series deal,2023-05-04,CNN
+"Leaders of Sudan’s warring factions agree to seven-day ceasefire, South Sudan says",2023-05-04,CNN
+Canadian man charged with aiding suicide by selling lethal substance online,2023-05-04,CNN
+"Brazilian police raid former President Bolsonaro’s home, arrest former aide: CNN Brasil",2023-05-04,CNN
+Venezuela’s Juan Guaidó seeks support in Washington after causing a stir in Colombia,2023-05-04,CNN
+"Cuba fuel shortages spark fistfights, queues and holiday cutbacks",2023-05-04,CNN
+American mountaineer dies during climb to Mount Everest,2023-05-04,CNN
+"Oil tanker catches fire off Malaysian coast, three crew missing",2023-05-04,CNN
+"Paetongtarn Shinawatra, frontrunner in heated Thai election, gives birth two weeks before polls open",2023-05-04,CNN
+Take a good look at these strange-looking turtles. They could soon be extinct,2023-05-04,CNN
+Everything you need to know about the Marriott Bonvoy rewards program,2023-05-04,CNN
+22 of the best hotels around the world for using Marriott free night certificates,2023-05-04,CNN
+"From hotel stays to unique experiences, here are the best ways to spend Marriott Bonvoy points",2023-05-04,CNN
+Italians judge Spanish pizzeria as the best in Europe – but there’s a catch,2023-05-04,CNN
+Swarm of bees delays a Delta flight by three hours,2023-05-04,CNN
+Historian claims to have located mystery ‘Mona Lisa’ bridge,2023-05-04,CNN
+Photographers dive into fundraising for ocean conservation,2023-05-04,CNN
+This Y2K pastime is becoming a big hit among South Korean youth,2023-05-04,CNN
+Jameela Jamil slams Met Gala for celebrating Karl Lagerfeld,2023-05-04,CNN
+Remember when Prince bared his butt at the 1991 VMAs?,2023-05-04,CNN
+Historic May storm brings snow and record cold temperatures to the Great Lakes,2023-05-04,CNN
+How a thunderstorm can produce a tornado,2023-05-04,CNN
+The Enhanced Fujita scale measures how strong tornadoes can get,2023-05-04,CNN
+"Severe storms featuring tornadoes, waterspouts possible along Gulf Coast this weekend",2023-05-04,CNN
+Seattle Kraken is rising to success since joining NHL,2023-05-04,CNN
+Russia accuses the United States of being behind the alleged drone attack on the Kremlin,2023-05-04,CNN
+What is the relevance of the monarchy in modern-day Britain?,2023-05-04,CNN
+Russia fires drones at Odesa,2023-05-04,CNN
+"Israel: suspects killed in attack on UK-Israeli mother, daughters",2023-05-04,CNN
+Serbia reels from mass school shooting that left nine people dead,2023-05-04,CNN
+"Man suspected of deadly shooting in Atlanta, Georgia captured after manhunt",2023-05-04,CNN
+Special counsel sat in on Pence testimony to federal grand jury,2023-05-04,CNN
+Ex-Russian lawmaker believes this is who carried out drone attack on Kremlin,2023-05-04,CNN
+Jury starts deliberating in Ed Sheeran copyright trial,2023-05-04,CNN
+Russia unleashes intense airstrikes against Ukraine after claiming Ukraine fired drones at Kremlin,2023-05-04,CNN
+India’s top female wrestlers are camping on the streets of New Delhi. Here’s why,2023-05-04,CNN
+World’s first 3D-printed mosque to be built in Dubai,2023-05-04,CNN
+"Yasmeen Lari, ‘starchitect’ turned social engineer, wins one of architecture’s most coveted prizes",2023-05-04,CNN
+"A massive, newly restored Frank Lloyd Wright mansion is up for sale",2023-05-04,CNN
+Are these the most sustainable architectural projects in the world?,2023-05-04,CNN
+Step inside a $72 million London townhouse in a converted army barracks,2023-05-04,CNN
+Napoli wins first Serie A title in 33 years after securing draw at Udinese,2023-05-04,CNN
+"Churchill Downs: After four horses die, home of Kentucky Derby calls circumstances ‘unacceptable’ and ‘troubling’",2023-05-04,CNN
+"Attorneys general announce probe into the NFL over workplace harassment, citing numerous complaints from women",2023-05-04,CNN
+"Jackson Mahomes, brother of NFL superstar Patrick, arrested and charged with sexual battery",2023-05-04,CNN
+These Ukrainian veterinarians are risking their lives to care for dogs and cats in the war zone,2023-05-04,CNN
+This CNN Hero is recruiting recreational divers to help rebuild reefs in Florida one coral at a time,2023-05-04,CNN
+This CNN Hero offers judgment-free veterinary care for the pets of those experiencing homelessness,2023-05-04,CNN
+Don’t give up on milestones: A CNN Hero’s message for Autism Awareness Month,2023-05-04,CNN
+US denies masterminding Moscow drone attack,2023-05-04,BBC
+US denies masterminding Moscow drone attack,2023-05-04,BBC
+Sudan generals unwilling to end fighting – UN,2023-05-04,BBC
+Silence and teddies at scene of Serbia school shooting,2023-05-04,BBC
+Several teachers killed in Pakistan school shooting,2023-05-04,BBC
+Italian uproar at French 'insults' on migration,2023-05-04,BBC
+Four Proud Boys guilty of seditious conspiracy,2023-05-04,BBC
+Ed Sheeran wins Thinking Out Loud copyright case,2023-05-04,BBC
+US denies masterminding Moscow drone attack,2023-05-04,BBC
+Sudan generals unwilling to end fighting – UN,2023-05-04,BBC
+Silence and teddies at scene of Serbia school shooting,2023-05-04,BBC
+Several teachers killed in Pakistan school shooting,2023-05-04,BBC
+Italian uproar at French 'insults' on migration,2023-05-04,BBC
+Four Proud Boys guilty of seditious conspiracy,2023-05-04,BBC
+Ed Sheeran wins Thinking Out Loud copyright case,2023-05-04,BBC
+Sudan generals unwilling to end fighting – UN,2023-05-04,BBC
+Silence and teddies at scene of Serbia school shooting,2023-05-04,BBC
+Several teachers killed in Pakistan school shooting,2023-05-04,BBC
+Italian uproar at French 'insults' on migration,2023-05-04,BBC
+Four Proud Boys guilty of seditious conspiracy,2023-05-04,BBC
+Ed Sheeran wins Thinking Out Loud copyright case,2023-05-04,BBC
+New York subway chokehold death sparks protests,2023-05-04,BBC
+Israel says it killed gunmen who shot UK-Israelis,2023-05-04,BBC
+Shoe-shop thieves get off on the wrong foot,2023-05-04,BBC
+Carrie Fisher family row eclipses Hollywood honour,2023-05-04,BBC
+Napoli win Italian title for first time in 33 years,2023-05-04,BBC
+Pioneering actress killed in Sudan cross-fire,2023-05-04,BBC
+Somali shock as mayor filmed lashing partygoers,2023-05-04,BBC
+Beaten and forced to flee Uganda for being gay,2023-05-04,BBC
+Searching for the truth about my missing father,2023-05-04,BBC
+What if King Charles was being crowned in Ghana?,2023-05-04,BBC
+Video 2 minutes 3 secondsRussian media's muted response to Kremlin explosion,2023-05-04,BBC
+Video 38 secondsWatch bundle of junk drift away in space,2023-05-04,BBC
+Video 2 minutes 8 secondsMeeting the would-be queen of Italy,2023-05-04,BBC
+Video 1 minute 48 secondsRos Atkins on... the videos showing 'Kremlin attack',2023-05-04,BBC
+Video 53 secondsOfficer inches from death as car spins across highway,2023-05-04,BBC
+The 'super-deep' diamonds in the crown,2023-05-04,BBC
+What side-hustlers are really making,2023-05-04,BBC
+A Mexican party that can't be missed,2023-05-04,BBC
+What kings wore from Tudor times to now,2023-05-04,BBC
+Do you own too many clothes?,2023-05-04,BBC
+Can remote-work gossip backfire?,2023-05-04,BBC
+The true story of the Kentucky Derby,2023-05-04,BBC
